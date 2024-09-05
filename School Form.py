a0bdd8c96c0303df66024922e72f3b648b62ce90
TreshBee18 <treasurecareink@gmail.com>
2024-09-05 09:22:39 -0700

    Add files via upload

---
+++ School Form.py
@@ -0,0 +1,25 @@
+# A function that multiply two numbers and get the result
+def get_multiply(num1=20, num2=5, multiply=20*5):
+    print('find the multiple of two values')
+    print(num1, "*", num2, "=", multiply)  
+get_multiply()
+
+
+ 
+# a function that multiply four numbers and get the result
+def get_multiply(num1=4, num2=5, num3=5, num4=10, multiply=4*5*5*10):
+    print('find the multiple of four values')
+    print(num1, "*", num2, "*", num3, "*", num4, "=", multiply)
+get_multiply()
+
+
+
+# a funtion that adds and subtracts two numbers and get the result
+def get_sum(num1=10, num2=50, add=10+50):
+    print('find the sum of two values')
+    print(num1, "+", num2, "+", subtract)
+    get_subtraction(num1={"result of (addtion)"}, num2= {"result of 20+20"})
+    print('find the difference of two values')
+    print(num1, "-", num2, "-", subtraction)
+    
+        
